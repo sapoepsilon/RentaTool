226084a2cf51fff76d3ef41af083e41bb223b9aa
Lucas L <nutnewbr@gmail.com>
2021-01-29 13:35:54 -0700

    Added MainPage with Navigation
    Added Main Paige with 2 buttons: Sign in and Sign Up.
    Created pages: SignIn,SignUp,Browse, Account, Add New Tool

---
+++ RentTool/Views/Account.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace RentTool
+{
+    public partial class Account : ContentPage
+    {
+        public Account()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RentTool/Views/Browse.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace RentTool
+{
+    public partial class Browse : ContentPage
+    {
+        public Browse()
+        {
+            InitializeComponent();
+        }
+    }
+}
