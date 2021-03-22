0e12d6f832a918238628d8f59b193104aa3bb6af
Lucas Lima <44297238+lucasmrl@users.noreply.github.com>
2021-03-22 10:55:52 -0600

    FIXED: Getting current user's address

--- RentTool/ViewModels/BrowseViewModel.cs
+++ RentTool/ViewModels/BrowseViewModel.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Windows.Input;
 using Firebase.Auth;
+using Newtonsoft.Json;
 using Plugin.CloudFirestore;
 using RentTool.Models;
 using Xamarin.Essentials;
@@ -20,6 +21,8 @@ namespace RentTool.ViewModels
         public string WebApiKey = "AIzaSyAUum5OozKcO7mXvgnXIQ7PLTC8vdmXMcI";
         private ObservableCollection<CardTool> items;
         private double miles;
+        private string userAddress;
+        public string UserID;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -35,12 +38,45 @@ namespace RentTool.ViewModels
         [Obsolete]
         public BrowseViewModel()
         {
-
+            getUsetZip();
             QueryRequest();
             Items = new ObservableCollection<CardTool>() {
         };
 
         }
+
+        [Obsolete]
+        async private void getUsetZip()
+        {
+            var authProvider = new FirebaseAuthProvider(new FirebaseConfig(WebApiKey));
+            try
+            {
+                //This is the saved firebaseauthentication that was saved during the time of login
+                var savedfirebaseauth =
+                    JsonConvert.DeserializeObject<Firebase.Auth.FirebaseAuth>(Preferences.Get("MyFirebaseRefreshToken",
+                        ""));
+                //Here we are Refreshing the token
+                var RefreshedContent = await authProvider.RefreshAuthAsync(savedfirebaseauth);
+                Preferences.Set("MyFirebaseRefreshToken", JsonConvert.SerializeObject(RefreshedContent));
+                //Now lets grab user information
+                UserID = savedfirebaseauth.User.LocalId;
+
+                var userDocument = await CrossCloudFirestore.Current
+                    .Instance
+                    .GetCollection("users")
+                    .GetDocument(UserID)
+                    .GetAsync();
+
+                var QueryObject = userDocument.ToObject<Models.user>();
+                userAddress = QueryObject.zip;
+
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Alert", ex.Message, "OK");
+            }
+        }
+
         [Obsolete]
         public async void QueryRequest()
         {
@@ -57,19 +93,11 @@ namespace RentTool.ViewModels
                 
                 foreach (var tool in yourModels)
                 {
-                    //Example to Calculate distance between 2 locations
-                    //Location boston = new Location(42.358056, -71.063611);
-                    //Location sanFrancisco = new Location(37.783333, -122.416667);
-                    //double miles = Location.CalculateDistance(boston, sanFrancisco, DistanceUnits.Miles);
-                    //Console.WriteLine(miles);
-
                     // 1) Grab user's location (lat, long)
 
                     try
                     {
-                        var addressUser = "84103";
-
-                        var locationsUser = await Geocoding.GetLocationsAsync(addressUser);
+                        var locationsUser = await Geocoding.GetLocationsAsync(userAddress);
                         var locationUser = locationsUser?.FirstOrDefault();
 
                         var locationsTool = await Geocoding.GetLocationsAsync(tool.toolAddress);
@@ -77,7 +105,7 @@ namespace RentTool.ViewModels
 
                         if (locationUser != null)
                         {
-                            Console.WriteLine($"Latitude: {locationUser.Latitude}, Longitude: {locationUser.Longitude}, Altitude: {locationUser.Altitude}");
+                            Console.WriteLine($"User zip code: {userAddress}");
                             Console.WriteLine($"Latitude: {locationTool.Latitude}, Longitude: {locationTool.Longitude}, Altitude: {locationTool.Altitude}");
                             Console.WriteLine("miles:");
                             miles = Location.CalculateDistance(locationUser, locationTool, DistanceUnits.Miles);
@@ -87,10 +115,12 @@ namespace RentTool.ViewModels
                     catch (FeatureNotSupportedException fnsEx)
                     {
                         // Feature not supported on device
+                        Console.WriteLine("NOT WORKING FOR THIS DEVICE");
                     }
                     catch (Exception ex)
                     {
                         // Handle exception that may have occurred in geocoding
+                        Console.WriteLine("SOMETHING HAPPENED WITH GEOLOCATION");
                     }
 
 
